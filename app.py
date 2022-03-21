9280ea42316b3d4afc40125b99e8f86b638460fc
tomkingchen <tomking.chen@gmail.com>
2022-03-21 19:07:35 +1100

    Switch to display hospitalisation only

--- app.py
+++ app.py
@@ -16,14 +16,18 @@ def hello():
     daily_summary_table = page_content.find('table', class_='DAILY-SUMMARY')
     notchecked = True
     for daily_summary_tr in daily_summary_table.find_all('tr')[1:]:
-        daily_newcases_cat = daily_summary_tr.find('td', class_='COL1 CATEGORY')
-        daily_newcases_cat = daily_newcases_cat.text.strip()
-        if daily_newcases_cat == "New Cases" and notchecked:
-            daily_newcases = daily_summary_tr.find('td', class_='COL2 TOTAL')
-            scrap_result = daily_newcases.text.strip()
-            notchecked = False
-        elif daily_newcases_cat == "Cases" and notchecked :
-            daily_newcases = daily_summary_tr.find('td', class_='COL4 NET')
-            scrap_result = daily_newcases.text.strip()
-            notchecked = False
+        daily_numbers = daily_summary_tr.find('td', class_='COL1 CATEGORY')
+        daily_numbers = daily_numbers.text.strip()
+        # if daily_numbers == "New Cases" and notchecked:
+        #     daily_newcases = daily_summary_tr.find('td', class_='COL2 TOTAL')
+        #     scrap_result = daily_newcases.text.strip()
+        #     notchecked = False
+        # elif daily_numbers == "Cases" and notchecked :
+        #     daily_newcases = daily_summary_tr.find('td', class_='COL4 NET')
+        #     scrap_result = daily_newcases.text.strip()
+        #     notchecked = False
+        if daily_numbers == "Hospitalised" and notchecked:
+            daily_hosp_number = daily_summary_tr.find('td', class_='COL2 TOTAL')
+            scrap_result = daily_hosp_number.text.strip()
+
     return "<head><style>p.bignumber { font-size: 330px;}</style></head><body><p class=\"bignumber\">" + scrap_result + "</p></body>"
